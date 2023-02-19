372ebbe8668cb4ca5ccffc52997ee5911eb2f027
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2023-02-18 20:20:55 -0600

    add iterative sorting functions

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,25 +6,48 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
 
-
+        for j in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    count = []
+    output = []
 
-    return arr
+    if arr == output:
+        return arr;
+
+    if maximum > 0:
+        count = [0 for _ in range(0, maximum)]
+    else:
+        count = [0 for _ in range(0, max(arr) + 1)]
+
+    for item in arr:
+        if item < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        count[item] += 1
+
+    for i in range(0, len(count)):
+        for j in range (0, count[i]):
+            output.append(i)
+    return output
