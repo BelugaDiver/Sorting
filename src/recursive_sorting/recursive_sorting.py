3c85719eb28ec4188026154dc8a5b5dd8bbb8ec6
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2023-02-21 09:04:10 -0600

    Add non-in-place merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,36 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
+    merged_arr = []
+
+    size = max(len(arrA), len(arrB))
+    while (len(arrA) > 0 or len(arrB) > 0):
+        if len(arrA) > 0 and len(arrB) > 0:
+            if (arrA[0] < arrB[0]):
+                merged_arr.append(arrA[0])
+                del arrA[0]
+            else:
+                merged_arr.append(arrB[0])
+                del arrB[0]
+        elif len(arrA) > 0:
+            merged_arr.append(arrA[0])
+            del arrA[0]
+        elif len(arrB) > 0:
+            merged_arr.append(arrB[0])
+            del arrB[0]
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if len(arr) <= 1:
+        return arr
 
-    return arr
+    length = len(arr)
+    midPoint = int((length - 1)/ 2)
+
+    merged_arr = merge(merge_sort(arr[0: midPoint + 1]), merge_sort(arr[midPoint + 1: length]))
+    return merged_arr
 
 
 # STRETCH: implement an in-place merge sort algorithm
