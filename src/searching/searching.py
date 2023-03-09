daa6c2ea514c511ef1bb36cf2391cde0256955e3
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2023-03-08 21:18:21 -0600

    add binary search iterative and recursive

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,11 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+  for i in range(len(arr)):
+    if target == arr[i]:
+      return i
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,6 +18,17 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  mid = 0
+
+  while high > low:
+    mid = int((high + low) / 2)
+    if arr[mid] > target:
+      high = mid
+    else:
+      if arr[mid] == target:
+        return mid
+
+      low = mid + 1
 
   return -1 # not found
 
@@ -28,3 +41,11 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  
+  if arr[middle] > target:
+      return binary_search_recursive(arr, target, low, middle)
+  else:
+    if arr[middle] == target:
+      return middle
+
+    return binary_search_recursive(arr, target, middle + 1, high)
